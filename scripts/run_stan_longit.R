ae0c90cee490ffe96b7d1bbc402b6c17109ccbfa
“ShozenD” <shozen.dan@keio.jp>
2023-12-16 22:50:25 +0000

    [add] HPC preparations

--- scripts/run_stan_longit.R
+++ scripts/run_stan_longit.R
@@ -1,93 +1,55 @@
+# ===== Load libraries =====
+library(yaml)
+library(optparse)
 library(readr)
+library(dplyr)
 library(cmdstanr)
-library(ggplot2)
 
-# Load data
+# Source functions
+source("R/make_stan_data.R")
+
+# ===== Load configurations =====
+# Parse command line arguments
+option_list <- list(
+  make_option(c("--config"), type = "character", default = NA, help = "configuration file", dest = "config_file")
+)
+cli_args <- parse_args(OptionParser(option_list = option_list))
+
+cat(" Loading configurations...\n")
+config <- read_yaml(file.path("config", cli_args$config_file))
+
+# ===== Load data =====
+cat(" Loading data...\n")
 data <- read_rds("data/silver/covimod-wave-3to12.rds")
 
-# Unpack data
-part_idx <- data$part_idx
-y <- data$y
-X <- data$X
+# ===== Prepare data =====
+cat(" Preparing Stan data...\n")
+stan_data <- make_stan_data(data, config)
 
-w_idx <- X[, "wave"] - 2 # Since we start from wave 3
-r_idx <- X[, "rep"] + 1 # Since we start with 0 repeats
-y_lag <- X[,"y_tot_lag"]
+# Save stan_data for convienient access from different scripts
+out_dir <- config$out_dir
+out_dir <- file.path(out_dir, "stan_data")
+if (!dir.exists(out_dir)) dir.create(out_dir, recursive = TRUE)
+saveRDS(stan_data, file = file.path(out_dir, paste0(config$model$name, ".rds")))
 
-X_dummies <- X[, -c(1,2,3)]
+# ===== Compile Stan model =====
+cat(" Compiling Stan model...\n")
+stan_model <- cmdstan_model(file.path("stan_models", paste0(config$model$name, ".stan")),
+                            compile = TRUE)
 
-# Create stan_data
-stan_data <- list(
-  N = length(y),
-  N_part = max(part_idx),
-  N_wave = max(w_idx),
-  N_repeat = max(r_idx),
-  P = ncol(X_dummies),
-  X = X_dummies,
-  part_idx = part_idx,
-  w_idx = w_idx,
-  r_idx = r_idx,
-  y_lag = y_lag,
-  y = y
-)
+# ===== Run MCMC =====
+cat(" Running MCMC...\n")
+stan_fit <- stan_model$sample(data = stan_data,
+                              seed = config$mcmc$seed, 
+                              chains = config$mcmc$chains,
+                              parallel_chains = config$mcmc$parallel_chains,
+                              iter_warmup = config$mcmc$iter_warmup,
+                              iter_sampling = config$mcmc$iter_sampling,
+                              max_treedepth = config$mcmc$max_treedepth)
 
-stan_model <- cmdstan_model("stan_models/pois_longit_hill.stan")
-
-fit <- stan_model$sample(data = stan_data,
-                         seed = 123,
-                         chains = 4,
-                         parallel_chains = 4,
-                         iter_warmup = 500,
-                         iter_sampling = 1000,
-                         max_treedepth = 11,
-                         refresh = 10)
-
-fit$save_object("../covimod-fatigue-outputs/stan_fits/pois_longit_hill.rds")
-
-fit$summary()
-fit$cmdstan_summary()
-
-post_samples <- fit$draws(variables = c("alpha", "tau"), format = "matrix")
-
-alpha <- post_samples[,colnames(post_samples) == "alpha"]
-tau <- post_samples[,colnames(post_samples) != "alpha"]
-lambda <- sweep(tau, 1, alpha, "+")
-
-lambda <- apply(lambda, 2, function(x) quantile(exp(x), probs = c(.025, .5, .975)))
-lambda <- t(lambda)
-
-colnames(lambda) <- c("CL", "M", "CU")
-
-df_lambda <- as.data.frame(lambda)
-df_lambda$wave <- 1:nrow(df_lambda)
-
-ggplot(df_lambda, aes(wave, M)) +
-  geom_line() +
-  geom_ribbon(aes(ymin = CL, ymax = CU), alpha = 0.3) +
-  theme_bw()
-
-post_samples <- fit$draws(variables = c("Kappa", "Slope"), format = "matrix")
-
-hill_params <- apply(post_samples, 2, function(x) quantile(x, probs = c(.025, .5, .975)))
-hill_params <- t(hill_params)
-colnames(hill_params) <- c("CL", "M", "CU")
-
-df_hill <- as.data.frame(hill_params)
-
-hill <- function(x, K, S) {
-  return( 1 / (1 + (K / x)^S) )
-}
-
-x <- seq(0,1,0.01)
-y <- hill(x, hill_params[1,2], hill_params[2,2])
-y_cl <- hill(x, hill_params[1,1], hill_params[2,1])
-y_cu <- hill(x, hill_params[1,3], hill_params[2,3])
-
-plot(x, y, type = "l")
-lines(x, y_cl)
-lines(x, y_cu)
-
-ggplot(df_rho, aes(wave, M)) +
-  geom_line() +
-  #geom_ribbon(aes(ymin = CL, ymax = CU), alpha = 0.3) +
-  theme_bw()
+# ===== Save fitted model =====
+cat(" Saving the fitted model...\n")
+# Setup output directory
+out_dir <- file.path(config$out_dir, "stan_fits")
+if (!dir.exists(out_dir)) dir.create(out_dir, recursive = TRUE)
+stan_fit$save_object(file.path(out_dir, paste0(config$model$name, ".rds")))
