adaf24957aa5565bca80f8530ea618032739bbe1
“ShozenD” <shozen.dan@keio.jp>
2023-12-17 14:54:44 +0000

    [add] script for installing dependencies

---
+++ install-dependencies-hpc.R
@@ -0,0 +1,28 @@
+# Install the required package dependencies
+cat("Installing dependencies...\n")
+
+# Install / load renv
+if (require("renv")) {
+  cat(" renv is installed and loaded")
+} else {
+  cat(" Installing renv...")
+  install.packages("renv")
+  if (require("renv")) {
+    cat(" renv is installed and loaded")
+  } else {
+    stop(" Failed to install renv")
+  }
+}
+
+# Restore the state of the project (i.e. install all dependencies)
+renv::restore()
+
+# Install cmdstanr and cmdstan
+cat(" Installing cmdstanr...\n")
+install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+
+require("cmdstanr")
+cmdstanr::check_cmdstan_toolchain()
+cmdstanr::install_cmdstan()
+
+cat(" DONE!\n")
