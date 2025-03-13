c6dc366762b5a14f1cd853b5cbe65a65664c1437
“ShozenD” <shozen.dan@keio.jp>
2025-03-13 15:06:14 +0000

    [add] HPC scripts

---
+++ hpc_scripts/run_stan_gam_wave_cap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+REPO_PATH="/rds/general/user/sd121/home/contact-survey-fatigue"
+OUT_PATH="/rds/general/user/sd121/home/contact-survey-fatigue-outputs"
+CONFIG_FILE="negb_gam_wave_cap10.yaml"
+
+# Create main script
+cat > "$OUT_PATH/run_stan_gam_wave_cap.pbs" <<EOF
+#!/bin/bash
+#PBS -l walltime=08:00:00
+#PBS -l select=1:ncpus=8:ompthreads=1:mem=50gb
+#PBS -J 1-33
+
+module load anaconda3/personal
+source activate contact-survey-fatigue
+
+# Move into repository
+cd $REPO_PATH
+
+# Run Stan model
+Rscript scripts/run_stan_gam_wave_cap.R --config "$CONFIG_FILE" --arr_idx \$PBS_ARRAY_INDEX
+EOF
+
+# Execute main script
+cd $OUT_PATH
+qsub "run_stan_gam_wave_cap.pbs"
