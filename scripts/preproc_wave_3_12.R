ee179bc57f08896166046af33a6fd690e1240c82
“ShozenD” <shozen.dan@keio.jp>
2025-03-30 17:23:15 +0100

    [update] preprocessing

--- scripts/preproc_wave_3_12.R
+++ scripts/preproc_wave_3_12.R
@@ -58,14 +58,10 @@ dt  <- dt[, urbn_type := case_when(URBN_TYPE == "1" ~ "Urban",
 # Calculate the total number of contacts
 dt[, y := y_hh + y_nhh + y_grp]
 
-<<<<<<< HEAD
 # Truncate the number of contacts at the 99th percentile
 q_99 <- quantile(dt$y, 0.99, na.rm = TRUE)
 cat("Truncating at the 99th percentile: ", q_99, "\n")
 dt[, y := pmin(y, q_99)]
-=======
-mean(dt$y > 30)
->>>>>>> d04f040fbc886779ea956befb4c2c3850f5ba5c6
 
 # Truncate household size at 4 (more than 90% of all households)
 dt[, hh_size := ifelse(hh_p_incl_0 > 5, 5, hh_p_incl_0)]
