2d92a9f2a11da11166b36568634b6777acdbe5bb
Shozen Dan <xiaoran.duan@gmail.com>
2025-04-15 07:40:44 +0100

    [add] urban rural weighting function

--- R/wcint.R
+++ R/wcint.R
@@ -1,18 +1,20 @@
-#' Weighted average of contact intensity by age, gender, and household size
+#' Weighted average of contact intensity by age, gender, household size, urban-rural typography
 #'
 #' @param age_set A vector of age groups
 #' @param draws_log_mu A draws_matrix of log contact intensity
 #' @param draws_beta_sex A draws_matrix of beta_sex (fixed effects)
 #' @param draws_beta_hhsize A draws_matrix of beta_hhsize (fixed effects)
+#' @param draws_beta_urbn A draws_matrix of beta_urbn (fixed effects)
 #'
 #' @return A draws_array for the weighted contact intensity
 #' @export
-wcint_agh <- function(age_set, draws_log_mu, draws_beta_sex, draws_beta_hhsize) {
-  log_mu <- wcint_age_gender(age_set, draws_log_mu, draws_beta_sex) # This function doesn't return a draws_matrix
-  log_mu <- wcint_hhsize(log_mu, draws_beta_hhsize)
-  draws_log_mu <- as_draws_matrix(matrix(log_mu, ncol=1))
+wcint_aghu <- function(age_set, draws_log_mu, draws_beta_sex, draws_beta_hhsize, draws_beta_urbn) {
+  log_wcint <- wcint_age_gender(age_set, draws_log_mu, draws_beta_sex) # This function doesn't return a draws_matrix
+  log_wcint <- wcint_hhsize(log_wcint, draws_beta_hhsize)
+  log_wcint <- wcint_urban_rural(log_wcint, draws_beta_urbn)
+  draws_log_wcint <- posterior::as_draws_matrix(matrix(log_wcint, ncol = 1))
 
-  return(draws_log_mu)
+  return(draws_log_wcint)
 }
 
 #' Weighted average of contact intensity by age and gender
@@ -46,28 +48,49 @@ wcint_age_gender <- function(age_set, draws_log_mu, draws_beta_sex) {
 
 #' Weighted average of contact intensity by household size
 #'
-#' @param draws_log_m a draws_matrix of log contact intensity, usually the output of wcint_age_gender.
+#' @param log_wcint a vector of log weighted contact intensity, usually the output of wcint_age_gender.
 #' @param draws_beta a draws_matrix of beta (fixed effects)
 #'
 #' @return A vector of draws for the weighted contact intensity
-wcint_hhsize <- function(draws_log_mu, draws_beta_hhsize) {
+wcint_hhsize <- function(log_wcint, draws_beta_hhsize) {
   # Load and process household size weights
-  hh_weights <- setDT(read_rds("data/population_weights/hhsize.rds"))$weight
+  hh_weights <- setDT(readr::read_rds("data/population_weights/hhsize.rds"))$weight
 
   # Calculate log_mu for different household sizes
-  log_mu_list <- lapply(1:5, function(i) {
+  log_wcint_list <- lapply(1:5, function(i) {
     if (i == 3) { # Reference group
-      draws_log_mu + log(hh_weights[i])
+      log_wcint + log(hh_weights[i])
     } else {
       if (i < 3) {
         adj <- as.numeric(draws_beta_hhsize[,i])
       } else {
         adj <- as.numeric(draws_beta_hhsize[,i - 1])
       }
-      draws_log_mu + adj + log(hh_weights[i])
+      log_wcint + adj + log(hh_weights[i])
     }
   })
-  log_mu <- log(Reduce(`+`, lapply(log_mu_list, exp)))
+  log_wcint <- log(Reduce(`+`, lapply(log_wcint_list, exp)))
 
-  return(log_mu)
+  return(log_wcint)
+}
+
+wcint_urban_rural <- function(log_wcint, draws_beta_urbn) {
+  # Load urban_rural weights
+  urbn_weights <- data.table::setDT(
+    readr::read_rds("../data/population_weights/urban_rural.rds")
+  )
+
+  log_wcint_list <- lapply(1:3, function(i){
+    if (i == 1) { # Reference group: urban
+      log_wcint + log(urbn_weights[urbn_type == "Urban"]$weight)
+    } else if (i == 2) {
+      adj <- as.numeric(draws_beta_urbn)
+      log_wcint + adj + log(urbn_weights[urbn_type == "Intermediate"]$weight)
+    } else {
+      adj <- as.numeric(draws_beta_urbn)
+      log_wcint - adj + log(urbn_weights[urbn_type == "Rural"]$weight)
+    }
+  })
+
+  log(Reduce(`+`, lapply(log_wcint_list, exp)))
 }
