8cd70c80ed4c4f1e8aab67c8257258b4208f7e54
Shozen Dan <xiaoran.duan@gmail.com>
2025-07-27 11:09:15 +0100

    [update] for reproducibility. Changed source data to Zenodo version.

--- scripts/preproc_varselect_wave_4.R
+++ scripts/preproc_varselect_wave_4.R
@@ -7,7 +7,7 @@ library(sf)
 
 # Load data
 repo_path <- getwd()
-covimod_data <- read_rds(file.path(repo_path, "data", "COVIMOD", "COVIMOD_data_2022-12-29.rds"))
+covimod_data <- read_rds(file.path(repo_path, "data", "COVIMOD", "COVIMOD_data_zenodo.rds"))
 nuts <- read_sf(file.path(repo_path, "data", "NUTS_RG_20M_2021_3035.geojson"))
 
 # Unpack data
@@ -84,5 +84,5 @@ df_cnt <- df_cnt %>% mutate(urbn_type = case_when(URBN_TYPE == 1 ~ "Urban",
 # ====================
 # Save data
 # ====================
-saveRDS(df_cnt, file.path("data", "silver", "covimod_wave_4.rds"))
+saveRDS(df_cnt, file.path("data", "covimod_wave_4.rds"))
 
